fb1061c41594eb3c5112dbbb6b0091c1a1cbfa57
shreyas-vgr <shreyas.vgr@gmail.com>
2014-11-03 12:05:48 +0530

    created template directory

--- music_recommendation/music/views.py
+++ music_recommendation/music/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from music.models import Songs
 # Create your views here.
+
+def index(request):
+	
+	results = Songs.objects.all()
+	output = ','.join([i.song_name for i in results])
+	return HttpResponse(output)
